6c3a8774840a1d46387bcce0c746b23e98a65c25
Kristoffer Kiil <krki@ssi.dk>
2021-09-02 15:14:24 +0200

    Fixed omponent name in __main__.py

--- bifrost_salmonella_subspecies_dtartrate/__main__.py
+++ bifrost_salmonella_subspecies_dtartrate/__main__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from bifrost_salmonella_subspecies_tartrate import launcher
+from bifrost_salmonella_subspecies_dtartrate import launcher
 
 if __name__ == '__main__':
     launcher.main()
