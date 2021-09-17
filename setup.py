90a8be4f57539fd0d23035a79ba8f2c0c6547a42
Kristoffer Kiil <krki@ssi.dk>
2021-09-17 23:50:07 +0200

    Bump version: v1_0_3__ → v1_0_4__

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bifrost_salmonella_subspecies_dtartrate',
-    version='v1_0_3__',
+    version='v1_0_4__',
     description='Enterobase component for salmonella serotyping',
     url='https://github.com/ssi-dk/bifrost_salmonella_subspecies_dtartrate',
     author="Kristoffer Kiil",
